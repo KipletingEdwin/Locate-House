9f1e041913b337544eec8f47b7802b1553bc5c54
Kipleting <kipletingedwin4@gmail.com>
2022-06-26 18:37:54 +0300

    add a display function

--- Assets/index.js
+++ Assets/index.js
@@ -18,3 +18,18 @@ function getTownMenu(){
     })
     .catch(error=>console.log(error))
     }
+
+    //Display house details of a specific town  in main section
+function displayInMainSection(event){
+    const clickedTownName = event.target.textContent;   
+    fetch('http://localhost:3000/houses')               
+    .then(response=>response.json())
+    .then(houses=>{
+    let sameTown =houses.find(house=>{
+                   return house.town === clickedTownName  // check if the name matches the grabbed one.    
+        });
+    updateHouse(sameTown)             // calling the function to display in main section
+    })
+    .catch(error=>console.log(error))
+    }
+
